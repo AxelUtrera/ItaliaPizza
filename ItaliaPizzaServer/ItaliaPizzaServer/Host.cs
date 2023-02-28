9e3b33d9497e432e0ca8f347989086797c026a47
Axel Utrera <axeljordanomu@gmail.com>
2023-02-28 16:12:54 -0600

    Entorno de desarrollo, division en capas y conexion cliente servidor

---
+++ ItaliaPizzaServer/ItaliaPizzaServer/Host.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItaliaPizzaServer
+{
+    internal class Host
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Comunication.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("The server is ready :D");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ItaliaPizzaServer/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1b62cf4-cb02-41f2-9a00-a5049c1a5c82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
