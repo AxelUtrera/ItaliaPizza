519572bb3cfa2e9b706214c1ad6fa3b5ed43a258
Axel Utrera <axeljordanomu@gmail.com>
2023-03-02 23:42:07 -0600

    Se agrego a la capa de acceso a datos EntityFramework y se realizo la configuracion correspondiente de la conexion a la base de datos

--- ItaliaPizza/View/Login.xaml.cs
+++ ItaliaPizza/View/Login.xaml.cs
@@ -18,9 +18,9 @@ namespace View
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
