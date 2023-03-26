256daf8c4486f2f4b0618dc5aaeccd3b402b886e
Dan <dansegura8863@gmail.com>
2023-03-25 20:43:23 -0600

    Casos de uso 24, 26 funcionando. CUs: 25 y 27 en proceso

---
+++ ItaliaPizza/Logic/IngredientLogic.cs
@@ -0,0 +1,166 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Data.Entity;
+using System.Windows;
+using System.Data.Entity.Infrastructure;
+using System.Security.Principal;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+using System.Data.Entity.Core;
+
+namespace Logic
+{
+    public class IngredientLogic
+    {
+        public static List<Ingredient> GetIngredients()
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+            try
+            {
+                using (ItaliaPizzaEntities context = new ItaliaPizzaEntities())
+                {
+                    var queryIngredients = context.ingredient.ToList();
+
+                    if (queryIngredients != null)
+                    {
+                        foreach (var aux in queryIngredients)
+                        {
+                            Ingredient ingredient = new Ingredient
+                            {
+                                IdIngredient = aux.idIngredient,
+                                IngredientName = aux.ingredientName,
+                                Quantity = aux.quantity
+                            };
+                            ingredients.Add(ingredient);
+                        }
+                    }
+                }
+            }
+            catch (EntityException ex)
+            {
+                throw new EntityException(ex.Message, ex);
+            }
+            return ingredients;
+        }
+
+        public static bool RegistIngredient(Ingredient ingredient)
+        {
+            using (ItaliaPizzaEntities context = new ItaliaPizzaEntities())
+            {
+                bool registered = false;
+                var ingredients = new DataAccess.ingredient
+                {
+                    ingredientName = ingredient.IngredientName,
+                    quantity = ingredient.Quantity,
+                    active = ingredient.IsActive
+                };
+                try
+                {
+                    context.ingredient.Add(ingredients);                   
+                    context.SaveChanges();
+                    registered= true;
+                }
+                catch (EntityException ex)
+                {                    
+                    throw new EntityException (ex.ToString());
+                }
+                return registered;
+            }
+        }
+        public static bool RegistRecipeIngredients(List<Ingredient> listIngredients, int idRecipe) 
+        {
+            bool response=false;
+            try
+            {
+                using (var context = new ItaliaPizzaEntities())
+                {
+                    foreach (var ingredient in listIngredients)
+                    {
+                        response = context.Database.ExecuteSqlCommand(
+                        "Insert into recipeIngredient(idIngredient,idRecipe, quantity) Values(@idIngredient, @idRecipe, @quantity)",
+                        new SqlParameter("idIngredient", ingredient.IdIngredient),
+                        new SqlParameter("idRecipe", idRecipe),
+                        new SqlParameter("quantity", ingredient.Quantity)
+                        ) != 0;
+                    }
+
+                }
+            }catch (SqlException ex)
+            {                
+                throw ex;
+            }
+            
+            return response;
+        }
+        public static bool DeleteRecipeIngredients(int idRecipe)
+        {
+            bool response = false;
+            try
+            {
+                using (var context = new ItaliaPizzaEntities())
+                {
+                    var foundRecipeIngredients = context.recipeIngredient.Where(x => x.idRecipe.Equals(idRecipe));
+                    context.recipeIngredient.RemoveRange(foundRecipeIngredients);
+                    context.SaveChanges();
+                }
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return response;
+            }
+        }
+        public static List<Ingredient> GetRecipeIngredients(int idRecipe)
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+            List<Ingredient> idIngredients = new List<Ingredient>();
+            try
+            {
+                SqlConnection connection = new SqlConnection("server=.\\SQLEXPRESS; database= ItaliaPizza; integrated security= true");
+                connection.Open();
+                SqlCommand command = new SqlCommand("Select * from recipeIngredient where idRecipe =" + idRecipe.ToString(), connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.IdIngredient = Convert.ToInt32(reader["idIngredient"]);
+                    ingredient.Quantity = Convert.ToInt32(reader["quantity"]);
+                    idIngredients.Add(ingredient);
+
+                }
+                foreach (var aux in idIngredients)
+                {
+                    using (var context = new ItaliaPizzaEntities())
+                    {
+                        var foundIngredient = (from ingredient in context.ingredient
+                                               where ingredient.idIngredient.Equals(aux.IdIngredient)
+                                               select ingredient).FirstOrDefault();
+
+                        if (foundIngredient != null)
+                        {
+                            Ingredient ingredient = new Ingredient()
+                            {
+                                IngredientName = foundIngredient.ingredientName,
+                                IdIngredient = foundIngredient.idIngredient,
+                                Quantity = aux.Quantity,
+                            };
+                            ingredients.Add(ingredient);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+            return ingredients;
+        }
+    }
+}
---
+++ ItaliaPizza/View/RecordRecipe.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Logic;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Lógica de interacción para RecordRecipe.xaml
+    /// </summary>
+    public partial class RecordRecipe : Window
+    {
+        private readonly List<Ingredient> selectedIngredients;
+        public RecordRecipe()
+        {
+            InitializeComponent();
+            ShowIngredients();
+            selectedIngredients = new List<Ingredient>();
+
+        }
+        private void Botton_Empty_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("¿Esta seguro de vaciar la lista de ingredientes seleccionados?", "Vaciar lista", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                selectedIngredients.Clear();
+                ListBox_SelectedIngredients.ItemsSource = null;
+            }
+        }
+        private void ShowIngredients()
+        {
+            _ = new List<Ingredient>();
+            List<Ingredient> ingredients = IngredientLogic.GetIngredients();
+            if (ingredients.Count > 0)
+            {
+                ListBox_Ingredients.ItemsSource = ingredients;
+            }
+            else
+            {
+                MessageBox.Show("No existen ingredientes registrados", "Ingredientes", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void ListBox_Ingredients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextBox_IngredientSelected.Text = ListBox_Ingredients.SelectedItem.ToString();
+        }
+
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox_Ingredients.SelectedItem != null)
+            {
+                if (TextBox_Amount.Text != null && int.TryParse(TextBox_Amount.Text, out int numericValue) && numericValue > 0)
+                {
+                    _ = new Ingredient();
+                    Ingredient ingredient = ListBox_Ingredients.SelectedItem as Ingredient;
+                    if (selectedIngredients.Contains(ingredient))
+                    {
+                        int index = selectedIngredients.IndexOf(ingredient);
+                        selectedIngredients[index].Quantity += numericValue;
+                    }
+                    else
+                    {
+                        ingredient.Quantity = numericValue;
+                        selectedIngredients.Add(ingredient);
+                    }
+                    ListBox_SelectedIngredients.ItemsSource = null;
+                    ListBox_SelectedIngredients.ItemsSource = selectedIngredients;
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, Ingrese una cantidad valida", "Valor invalido", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, Selecciona un ingrediente primero", "Ingredientes", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox_SelectedIngredients.SelectedItem != null)
+            {
+                _ = new Ingredient();
+                Ingredient ingredient = ListBox_SelectedIngredients.SelectedItem as Ingredient;
+                selectedIngredients.Remove(ingredient);
+                ListBox_SelectedIngredients.ItemsSource = null;
+                ListBox_SelectedIngredients.ItemsSource = selectedIngredients;
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un ingrediente a eliminar por favor");
+            }
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange textRange = new TextRange(RichTextBox_Description.Document.ContentStart,
+                RichTextBox_Description.Document.ContentEnd);
+            int validateResult = ValidateText();
+            if (validateResult == 1)
+            {
+
+                Recipe recipe = new Recipe()
+                {
+                    NameRecipe = TextBox_Tittle.Text,
+                    DescriptionRecipe = textRange.Text,
+                    IsActive = true,
+                };
+                int idRecipe;
+                switch (Logic.RecipeLogic.AllreadyExist(recipe.NameRecipe))
+                {
+                    case "exist":                        
+                        MessageBox.Show("Ya se encuentra una receta registrada con este mismo nombre","Receta Registrada",MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+
+                    case "doesNotExist":
+                        Logic.RecipeLogic.RegistRecipe(recipe);
+                        idRecipe = Logic.RecipeLogic.GetIdRecipe(recipe.NameRecipe);
+
+                        if (idRecipe > 0)
+                        {
+                            if (IngredientLogic.RegistRecipeIngredients(selectedIngredients, idRecipe))
+                            {
+                                MessageBox.Show("Registro Exitoso");
+                            }
+                            else
+                            {
+                                MessageBox.Show("El registro no pudo ser realizado", "Registro", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("El registro no pudo ser realizado", "Registro", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;                       
+
+                    case "idle":
+                        idRecipe = RecipeLogic.GetIdRecipe(recipe.NameRecipe);
+                        var result = MessageBox.Show("Esta receta ya se encuentra registrada pero inactiva.\n ¿Deseas reactivarla?", "Receta Inactiva", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            Logic.RecipeLogic.ActivateRecipe(idRecipe);
+                            var result2 = MessageBox.Show("¿Deseas guardar los cambios en la receta?.", "Guardar cambios", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                            if (result2 == MessageBoxResult.Yes)
+                            {
+                                MessageBox.Show(recipe.IdRecipe.ToString());
+                                RecipeLogic.EditRecipe(recipe);
+                                IngredientLogic.DeleteRecipeIngredients(idRecipe);
+                                IngredientLogic.RegistRecipeIngredients(selectedIngredients, idRecipe);
+                            }
+                        }
+                        break;
+                }
+            }
+            else if (validateResult == 0)
+            {
+                MessageBox.Show("Por favor, no ingreses caracteres extraños","Caracteres invalidos",MessageBoxButton.OK,MessageBoxImage.Warning);
+                RichTextBox_Description.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, llena todos los campos","Campos vacios",MessageBoxButton.OK, MessageBoxImage.Warning);
+                RichTextBox_Description.Focus();
+            }
+        }
+        private int ValidateText()
+        {
+            TextRange textRange = new TextRange(RichTextBox_Description.Document.ContentStart,
+               RichTextBox_Description.Document.ContentEnd);
+            int result=0;
+            if (!string.IsNullOrWhiteSpace(TextBox_Tittle.Text) && !string.IsNullOrWhiteSpace(textRange.Text) && selectedIngredients!=null)
+            {
+                Regex regex = new Regex(@"^(?=.?[#?!@$%^&-])");
+                Match match = regex.Match(textRange.Text);
+                Match match1 = regex.Match(TextBox_Tittle.Text);
+                if (!match.Success && !match1.Success)
+                {
+                    result = 1;
+                }
+            }
+            else
+            {
+                result = 2;
+            }
+            return result;
+        }
+    }
+}
