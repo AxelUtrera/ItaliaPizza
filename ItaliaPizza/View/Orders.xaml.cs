39ea1560849c1aba63c9b200a9a55d5b6b056d36
Axel Utrera <axeljordanomu@gmail.com>
2023-05-07 02:58:27 -0600

    Creacion de ventana ver pedidos y registrar pedido (sin probar ni ejecutar, solo realizadas)

---
+++ ItaliaPizza/View/Orders.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Lógica de interacción para Orders.xaml
+    /// </summary>
+    public partial class Orders : Window
+    {
+        public Orders()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Texbox_TextSearchChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void OrdersTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Update_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Edit_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Cancel_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
